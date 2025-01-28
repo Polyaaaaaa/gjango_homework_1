6324cf444b7b45f4934da71918cd652cfe0adff1
Polyaaaaaa <polinasatrajkina@gmail.com>
2025-01-28 19:34:37 +0300

    final commit of 27's homework

--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    country = models.CharField(max_length=15, blank=True, null=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
 
     USERNAME_FIELD = 'email'
